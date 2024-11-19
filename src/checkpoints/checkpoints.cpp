98fb73ca1c7336d53b22d2bf6f8a60cde7510f54
framepix <187810480+framepix@users.noreply.github.com>
2024-11-19 22:55:56 +0000

    updates

--- src/checkpoints/checkpoints.cpp
+++ src/checkpoints/checkpoints.cpp
@@ -62,6 +62,8 @@ namespace cryptonote
   height_to_hash const HARDCODED_MAINNET_CHECKPOINTS[] =
   {
     {0,      "e9a4e09fa64d650fd72d713c56172fa7e85ce43727b66c21396bc69daa89db28"},
+    {100,    "16a363d87def097e864e4d6e173b8dc0e4818ab5027c3023871b0c11aeceb101"},
+
   };
 
   crypto::hash get_newest_hardcoded_checkpoint(cryptonote::network_type nettype, uint64_t *height)
--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -1,5 +1,4 @@
 // Copyright (c) 2014-2019, The Monero Project
-// Copyright (c)      2018, The Guus Project
 //
 // All rights reserved.
 //
@@ -5276,7 +5275,7 @@ void Blockchain::cancel()
 }
 
 #if defined(PER_BLOCK_CHECKPOINT)
-static const char expected_block_hashes_hash[] = "8754309c4501f4b1c547c5c14d41dca30e0836a2942b09584ccc43b287040d07";
+static const char expected_block_hashes_hash[] = "df3f619804a92fdb4057192dc43dd748ea778adc52bc498ce80524c014b81119";
 void Blockchain::load_compiled_in_block_hashes(const GetCheckpointsCallback& get_checkpoints)
 {
   if (get_checkpoints == nullptr || !m_fast_sync)
