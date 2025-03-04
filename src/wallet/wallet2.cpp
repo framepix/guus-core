7daca0f7ea94dbdbaa241d6cd12c16d808c96d7b
Guus <187810480+framepix@users.noreply.github.com>
2025-03-04 03:47:05 -0800

    Update wallet2.cpp

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -9589,221 +9589,145 @@ void wallet2::get_outs(std::vector<std::vector<tools::wallet2::get_outs_entry>> 
 }
 
 void wallet2::transfer_selected_rct(std::vector<cryptonote::tx_destination_entry> dsts, const std::vector<size_t>& selected_transfers, size_t fake_outputs_count,
-  std::vector<std::vector<tools::wallet2::get_outs_entry>> &outs,
-  uint64_t unlock_time, uint64_t fee, const std::vector<uint8_t>& extra, cryptonote::transaction& tx, pending_tx &ptx, const rct::RCTConfig &rct_config, const guus_construct_tx_params &tx_params)
+    std::vector<std::vector<tools::wallet2::get_outs_entry>>& outs,
+    uint64_t unlock_time, uint64_t fee, const std::vector<uint8_t>& extra, cryptonote::transaction& tx, pending_tx& ptx,
+    const rct::RCTConfig& rct_config, const guus_construct_tx_params& tx_params)
 {
-  using namespace cryptonote;
-  // throw if attempting a transaction with no destinations
-  THROW_WALLET_EXCEPTION_IF(dsts.empty(), error::zero_destination);
+    using namespace cryptonote;
+    THROW_WALLET_EXCEPTION_IF(dsts.empty(), error::zero_destination);
 
-  uint64_t upper_transaction_weight_limit = get_upper_transaction_weight_limit();
-  uint64_t needed_money = fee;
-  LOG_PRINT_L2("transfer_selected_rct: starting with fee " << print_money (needed_money));
-  LOG_PRINT_L2("selected transfers: " << strjoin(selected_transfers, " "));
+    uint64_t upper_transaction_weight_limit = get_upper_transaction_weight_limit();
+    uint64_t needed_money = fee;
+    LOG_PRINT_L2("transfer_selected_rct: starting with fee " << print_money(needed_money));
+    LOG_PRINT_L2("selected transfers: " << strjoin(selected_transfers, " "));
 
-  // calculate total amount being sent to all destinations
-  // throw if total amount overflows uint64_t
-  for(auto& dt: dsts)
-  {
-    THROW_WALLET_EXCEPTION_IF(0 == dt.amount && (tx_params.tx_type != txtype::guus_name_system), error::zero_destination);
-    needed_money += dt.amount;
-    LOG_PRINT_L2("transfer: adding " << print_money(dt.amount) << ", for a total of " << print_money (needed_money));
-    THROW_WALLET_EXCEPTION_IF(needed_money < dt.amount, error::tx_sum_overflow, dsts, fee, m_nettype);
-  }
+    for(auto& dt: dsts) {
+        THROW_WALLET_EXCEPTION_IF(0 == dt.amount && (tx_params.tx_type != txtype::guus_name_system), error::zero_destination);
+        needed_money += dt.amount;
+        LOG_PRINT_L2("transfer: adding " << print_money(dt.amount) << ", for a total of " << print_money(needed_money));
+        THROW_WALLET_EXCEPTION_IF(needed_money < dt.amount, error::tx_sum_overflow, dsts, fee, m_nettype);
+    }
 
-  // if this is a multisig wallet, create a list of multisig signers we can use
-  std::deque<crypto::public_key> multisig_signers;
-  size_t n_multisig_txes = 0;
-  std::vector<std::unordered_set<crypto::public_key>> ignore_sets;
-  if (m_multisig && !m_transfers.empty())
-  {
-    const crypto::public_key local_signer = get_multisig_signer_public_key();
-    size_t n_available_signers = 1;
-
-    // At this step we need to define set of participants available for signature,
-    // i.e. those of them who exchanged with multisig info's
-    for (const crypto::public_key &signer: m_multisig_signers)
-    {
-      if (signer == local_signer)
-        continue;
-      for (const auto &i: m_transfers[0].m_multisig_info)
-      {
-        if (i.m_signer == signer)
-        {
-          multisig_signers.push_back(signer);
-          ++n_available_signers;
-          break;
+    std::deque<crypto::public_key> multisig_signers;
+    size_t n_multisig_txes = 0;
+    std::vector<std::unordered_set<crypto::public_key>> ignore_sets;
+    if (m_multisig && !m_transfers.empty()) {
+        const crypto::public_key local_signer = get_multisig_signer_public_key();
+        size_t n_available_signers = 1;
+        for (const crypto::public_key& signer : m_multisig_signers) {
+            if (signer == local_signer) continue;
+            for (const auto& i : m_transfers[0].m_multisig_info) {
+                if (i.m_signer == signer) {
+                    multisig_signers.push_back(signer);
+                    ++n_available_signers;
+                    break;
+                }
+            }
         }
-      }
+        THROW_WALLET_EXCEPTION_IF(n_available_signers < m_multisig_threshold, error::multisig_import_needed);
+        if (n_available_signers > m_multisig_threshold) {
+            tools::Combinator<crypto::public_key> c(std::vector<crypto::public_key>(multisig_signers.begin(), multisig_signers.end()));
+            auto ignore_combinations = c.combine(multisig_signers.size() + 1 - m_multisig_threshold);
+            for (const auto& combination : ignore_combinations) {
+                ignore_sets.push_back(std::unordered_set<crypto::public_key>(combination.begin(), combination.end()));
+            }
+            n_multisig_txes = ignore_sets.size();
+        } else {
+            n_multisig_txes = 1;
+        }
     }
-    // n_available_signers includes the transaction creator, but multisig_signers doesn't
-    MDEBUG("We can use " << n_available_signers << "/" << m_multisig_signers.size() <<  " other signers");
-    THROW_WALLET_EXCEPTION_IF(n_available_signers < m_multisig_threshold, error::multisig_import_needed);
-    if (n_available_signers > m_multisig_threshold)
-    {
-      // If there more potential signers (those who exchanged with multisig info)
-      // than threshold needed some of them should be skipped since we don't know
-      // who will sign tx and who won't. Hence we don't contribute their LR pairs to the signature.
 
-      // We create as many transactions as many combinations of excluded signers may be.
-      // For example, if we have 2/4 wallet and wallets are: A, B, C and D. Let A be
-      // transaction creator, so we need just 1 signature from set of B, C, D.
-      // Using "excluding" logic here we have to exclude 2-of-3 wallets. Combinations go as follows:
-      // BC, BD, and CD. We save these sets to use later and counting the number of required txs.
-      tools::Combinator<crypto::public_key> c(std::vector<crypto::public_key>(multisig_signers.begin(), multisig_signers.end()));
-      auto ignore_combinations = c.combine(multisig_signers.size() + 1 - m_multisig_threshold);
-      for (const auto& combination: ignore_combinations)
-      {
-        ignore_sets.push_back(std::unordered_set<crypto::public_key>(combination.begin(), combination.end()));
-      }
-
-      n_multisig_txes = ignore_sets.size();
+    uint64_t found_money = 0;
+    uint32_t subaddr_account = 0;
+    bool has_rct = false;
+    for (size_t i = 0; i < selected_transfers.size(); i++) {
+        size_t transfer_idx = selected_transfers[i];
+        transfer_details const& td = m_transfers[transfer_idx];
+        has_rct |= td.is_rct();
+        found_money += td.amount();
+        if (i == 0) subaddr_account = td.m_subaddr_index.major;
+        else THROW_WALLET_EXCEPTION_IF(subaddr_account != td.m_subaddr_index.major, error::wallet_internal_error, "the tx uses funds from multiple accounts");
     }
-    else
-    {
-      // If we have exact count of signers just to fit in threshold we don't exclude anyone and create 1 transaction
-      n_multisig_txes = 1;
+    LOG_PRINT_L2("wanted " << print_money(needed_money) << ", found " << print_money(found_money) << ", fee " << print_money(fee));
+    THROW_WALLET_EXCEPTION_IF(found_money < needed_money, error::not_enough_unlocked_money, found_money, needed_money - fee, fee);
+
+    if (outs.empty()) get_outs(outs, selected_transfers, fake_outputs_count, has_rct);
+
+    std::vector<cryptonote::tx_source_entry> sources;
+    std::unordered_set<rct::key> used_L;
+    for(size_t idx : selected_transfers) {
+        sources.push_back({});
+        cryptonote::tx_source_entry& src = sources.back();
+        const transfer_details& td = m_transfers[idx];
+        src.amount = td.amount();
+        src.rct = td.is_rct();
+
+        THROW_WALLET_EXCEPTION_IF(outs.size() < sources.size(), error::wallet_internal_error, "outs.size() < sources.size()");
+        THROW_WALLET_EXCEPTION_IF(outs[sources.size() - 1].size() < fake_outputs_count, error::wallet_internal_error, "fake_outputs_count > random outputs found");
+
+        for (size_t n = 0; n < fake_outputs_count + 1; ++n) {
+            tx_source_entry::output_entry oe;
+            oe.first = std::get<0>(outs[sources.size() - 1][n]);
+            oe.second.dest = rct::pk2rct(std::get<1>(outs[sources.size() - 1][n]));
+            oe.second.mask = std::get<2>(outs[sources.size() - 1][n]);
+            src.outputs.push_back(oe);
+        }
+
+        auto it_to_replace = std::find_if(src.outputs.begin(), src.outputs.end(), [&](const tx_source_entry::output_entry& a) {
+            return a.first == td.m_global_output_index;
+        });
+        THROW_WALLET_EXCEPTION_IF(it_to_replace == src.outputs.end(), error::wallet_internal_error, "real output not found");
+
+        tx_source_entry::output_entry real_oe;
+        real_oe.first = td.m_global_output_index;
+        real_oe.second.dest = rct::pk2rct(td.get_public_key());
+        real_oe.second.mask = rct::commit(td.amount(), td.m_mask);
+        *it_to_replace = real_oe;
+        src.real_out_tx_key = get_tx_pub_key_from_extra(td.m_tx, td.m_pk_index);
+        src.real_out_additional_tx_keys = get_additional_tx_pub_keys_from_extra(td.m_tx);
+        src.real_output = it_to_replace - src.outputs.begin();
+        src.real_output_in_tx_index = td.m_internal_output_index;
+        src.mask = td.m_mask;
+        src.multisig_kLRki = m_multisig ? get_multisig_composite_kLRki(idx, ignore_sets.empty() ? std::unordered_set<crypto::public_key>() : ignore_sets.front(), used_L, used_L)
+                                       : rct::multisig_kLRki({rct::zero(), rct::zero(), rct::zero(), rct::zero()});
     }
-    MDEBUG("We will create " << n_multisig_txes << " txes");
-  }
 
-  uint64_t found_money = 0;
-  uint32_t subaddr_account = 0;
-  bool has_rct = false;
-  for (size_t i = 0; i < selected_transfers.size(); i++)
-  {
-    size_t transfer_idx        = selected_transfers[i];
-    transfer_details const &td = m_transfers[transfer_idx];
-    has_rct                   |= td.is_rct();
-    found_money               += td.amount();
-
-    if (i == 0)
-      subaddr_account = m_transfers[transfer_idx].m_subaddr_index.major;
-    else
-      THROW_WALLET_EXCEPTION_IF(subaddr_account != m_transfers[transfer_idx].m_subaddr_index.major, error::wallet_internal_error, "the tx uses funds from multiple accounts");
-  }
-  LOG_PRINT_L2("wanted " << print_money(needed_money) << ", found " << print_money(found_money) << ", fee " << print_money(fee));
-  THROW_WALLET_EXCEPTION_IF(found_money < needed_money, error::not_enough_unlocked_money, found_money, needed_money - fee, fee);
-
-  if (outs.empty())
-    get_outs(outs, selected_transfers, fake_outputs_count, has_rct); // may throw
-
-  //prepare inputs
-  LOG_PRINT_L2("preparing outputs");
-  size_t i = 0, out_index = 0;
-  std::vector<cryptonote::tx_source_entry> sources;
-  std::unordered_set<rct::key> used_L;
-  for(size_t idx: selected_transfers)
-  {
-    sources.resize(sources.size()+1);
-    cryptonote::tx_source_entry& src = sources.back();
-    const transfer_details& td = m_transfers[idx];
-    src.amount = td.amount();
-    src.rct = td.is_rct();
-    //paste mixin transaction
-
-    THROW_WALLET_EXCEPTION_IF(outs.size() < out_index + 1 ,  error::wallet_internal_error, "outs.size() < out_index + 1"); 
-    THROW_WALLET_EXCEPTION_IF(outs[out_index].size() < fake_outputs_count ,  error::wallet_internal_error, "fake_outputs_count > random outputs found");
-      
-    typedef cryptonote::tx_source_entry::output_entry tx_output_entry;
-    for (size_t n = 0; n < fake_outputs_count + 1; ++n)
-    {
-      tx_output_entry oe;
-      oe.first = std::get<0>(outs[out_index][n]);
-      oe.second.dest = rct::pk2rct(std::get<1>(outs[out_index][n]));
-      oe.second.mask = std::get<2>(outs[out_index][n]);
-      src.outputs.push_back(oe);
+    std::vector<cryptonote::tx_destination_entry> splitted_dsts = dsts;
+    cryptonote::tx_destination_entry change_dts{};
+    change_dts.amount = found_money - needed_money;
+    bool update_splitted_dsts = true;
+    if (change_dts.amount == 0) {
+        if (splitted_dsts.size() == 1 || tx_params.tx_type == txtype::guus_name_system) {
+            cryptonote::account_base dummy;
+            dummy.generate();
+            change_dts.addr = dummy.get_keys().m_account_address;
+        } else {
+            update_splitted_dsts = false;
+        }
+    } else {
+        change_dts.addr = get_subaddress({subaddr_account, 0});
+        change_dts.is_subaddress = subaddr_account != 0;
     }
-    ++i;
-
-    //paste real transaction to the random index
-    auto it_to_replace = std::find_if(src.outputs.begin(), src.outputs.end(), [&](const tx_output_entry& a)
-    {
-      return a.first == td.m_global_output_index;
-    });
-    THROW_WALLET_EXCEPTION_IF(it_to_replace == src.outputs.end(), error::wallet_internal_error,
-        "real output not found");
-
-    tx_output_entry real_oe;
-    real_oe.first = td.m_global_output_index;
-    real_oe.second.dest = rct::pk2rct(td.get_public_key());
-    real_oe.second.mask = rct::commit(td.amount(), td.m_mask);
-    *it_to_replace = real_oe;
-    src.real_out_tx_key = get_tx_pub_key_from_extra(td.m_tx, td.m_pk_index);
-    src.real_out_additional_tx_keys = get_additional_tx_pub_keys_from_extra(td.m_tx);
-    src.real_output = it_to_replace - src.outputs.begin();
-    src.real_output_in_tx_index = td.m_internal_output_index;
-    src.mask = td.m_mask;
-    if (m_multisig)
-    {
-      auto ignore_set = ignore_sets.empty() ? std::unordered_set<crypto::public_key>() : ignore_sets.front();
-      src.multisig_kLRki = get_multisig_composite_kLRki(idx, ignore_set, used_L, used_L);
+    if (update_splitted_dsts) {
+        if (tx_params.tx_type == txtype::guus_name_system) {
+            assert(splitted_dsts.size() == 1);
+            splitted_dsts.back() = change_dts;
+        } else {
+            splitted_dsts.push_back(change_dts);
+        }
     }
-    else
-      src.multisig_kLRki = rct::multisig_kLRki({rct::zero(), rct::zero(), rct::zero(), rct::zero()});
-    detail::print_source_entry(src);
-    ++out_index;
-  }
-  LOG_PRINT_L2("outputs prepared");
 
-  // we still keep a copy, since we want to keep dsts free of change for user feedback purposes
-  std::vector<cryptonote::tx_destination_entry> splitted_dsts = dsts;
-  cryptonote::tx_destination_entry change_dts                 = {};
-  change_dts.amount                                           = found_money - needed_money;
-  bool update_splitted_dsts                                   = true;
-  if (change_dts.amount == 0)
-  {
-    if (splitted_dsts.size() == 1 || tx.type == txtype::guus_name_system)
-    {
-      // If the change is 0, send it to a random address, to avoid confusing
-      // the sender with a 0 amount output. We send a 0 amount in order to avoid
-      // letting the destination be able to work out which of the inputs is the
-      // real one in our rings
-
-      LOG_PRINT_L2("generating dummy address for 0 change");
-      cryptonote::account_base dummy;
-      dummy.generate();
-      LOG_PRINT_L2("generated dummy address for 0 change");
-      change_dts.addr = dummy.get_keys().m_account_address;
+    crypto::secret_key tx_key;
+    std::vector<crypto::secret_key> additional_tx_keys;
+    rct::multisig_out msout;
+    LOG_PRINT_L2("Constructing tx with sources: " << sources.size() << ", dests: " << splitted_dsts.size() << ", fee: " << print_money(fee));
+    bool r = cryptonote::construct_tx_and_get_tx_key(m_account.get_keys(), m_subaddresses, sources, splitted_dsts, change_dts, extra, tx, unlock_time, tx_key, additional_tx_keys, rct_config, m_multisig ? &msout : NULL, tx_params);
+    if (!r) {
+        LOG_ERROR("Failed to construct transaction: sources=" << sources.size() << ", dests=" << splitted_dsts.size() << ", fee=" << print_money(fee));
     }
-    else
-    {
-      update_splitted_dsts = false;
-    }
-  }
-  else
-  {
-    change_dts.addr = get_subaddress({subaddr_account, 0});
-    change_dts.is_subaddress = subaddr_account != 0;
-  }
-
-  if (update_splitted_dsts)
-  {
-    // NOTE: If LNS, there's already a dummy destination entry in there that
-    // we placed in (for fake calculating the TX fees and parts) that we
-    // repurpose for change after the fact.
-    if (tx_params.tx_type == txtype::guus_name_system)
-    {
-      assert(splitted_dsts.size() == 1);
-      splitted_dsts.back() = change_dts;
-    }
-    else
-    {
-      splitted_dsts.push_back(change_dts);
-    }
-  }
-
-  crypto::secret_key tx_key;
-  std::vector<crypto::secret_key> additional_tx_keys;
-  rct::multisig_out msout;
-  LOG_PRINT_L2("constructing tx");
-  auto sources_copy = sources;
-  bool r = cryptonote::construct_tx_and_get_tx_key(m_account.get_keys(), m_subaddresses, sources, splitted_dsts, change_dts, extra, tx, unlock_time, tx_key, additional_tx_keys, rct_config, m_multisig ? &msout : NULL, tx_params);
-
-  LOG_PRINT_L2("constructed tx, r="<<r);
-  THROW_WALLET_EXCEPTION_IF(!r, error::tx_not_constructed, sources, dsts, unlock_time, m_nettype);
-  THROW_WALLET_EXCEPTION_IF(upper_transaction_weight_limit <= get_transaction_weight(tx), error::tx_too_big, tx, upper_transaction_weight_limit);
-
+    THROW_WALLET_EXCEPTION_IF(!r, error::tx_not_constructed, sources, dsts, unlock_time, m_nettype);
+    THROW_WALLET_EXCEPTION_IF(upper_transaction_weight_limit <= get_transaction_weight(tx), error::tx_too_big, tx, upper_transaction_weight_limit);
+  
   // work out the permutation done on sources
   std::vector<size_t> ins_order;
   for (size_t n = 0; n < sources.size(); ++n)
